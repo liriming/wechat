77999be91e1d11651097dafd3a86a4c68bd9295c
liriming <594509671@qq.com>
2018-03-11 16:25:47 +0800

    diskid

--- src/main/java/com/iosre/pphb/service/PPService.java
+++ src/main/java/com/iosre/pphb/service/PPService.java
@@ -168,9 +168,9 @@ public class PPService {
                 Map<String, Object> contentMap = jsonMapper.readValue(content, Map.class);
                 if (!StringUtils.isEmpty(contentMap.get("idfv").toString())) {
                     String idfv = contentMap.get("idfv").toString();
-                    if(contentMap.containsKey("disk_id")){
+                  /*  if(contentMap.containsKey("disk_id")){
                         return contentMap.get("disk_id").toString();
-                    }
+                    }*/
 
                     String username = userDao.getUsername(idfv);
                     if (StringUtils.isEmpty(username) && contentMap.containsKey("username")) {
