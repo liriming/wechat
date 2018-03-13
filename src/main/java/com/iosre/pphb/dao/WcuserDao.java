426854ee910b0a286cdfbe2b1fc431cfdc5ec210
liriming <594509671@qq.com>
2018-03-13 16:56:46 +0800

    usphone

---
+++ src/main/java/com/iosre/pphb/dao/WcuserDao.java
@@ -0,0 +1,16 @@
+package com.iosre.pphb.dao;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+import java.util.Map;
+
+public interface WcuserDao {
+
+    @Insert("insert into wcuser(name,psw,_62,phoneno,isalive) values (#{name},#{psw},#{_62},#{phoneno},1)")
+    int insertDataInfo(@Param("name")String name,@Param("psw")String psw,@Param("_62")String _62,@Param("phoneno")String phoneno);
+
+}
