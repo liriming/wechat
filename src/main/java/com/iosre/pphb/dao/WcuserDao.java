0ef3fda273436739a162aa4aa339170c3d19d9f3
liriming <594509671@qq.com>
2018-05-15 10:16:09 +0800

    导出

--- src/main/java/com/iosre/pphb/dao/WcuserDao.java
+++ src/main/java/com/iosre/pphb/dao/WcuserDao.java
@@ -93,13 +93,13 @@ public interface WcuserDao {
 
 
     @Select("select a.id,a.name,a.psw,concat(left(a._62,50),'...') as _62,a.export,a.realname,a.checkpho,a.ctime,a.exporttime,a.ip," +
-            "b.ip as reip from wcuser a,wcphone b where (_62 is not null or _62 != '') and isalive=1 and export=#{export} and realname=#{realname} and checkpho=#{checkpho} " +
-            "and a.ctime BETWEEN #{bTime} AND  #{eTime} and right(name,11) like '1%' AND RIGHT(a.name,10) = right(b.phone,10) order by a.ctime ${sort} limit #{count} ")
+            "b.ip as reip from wcuser a LEFT JOIN wcphone b ON RIGHT (a.NAME, 10) = RIGHT(b.phone, 10) where (_62 is not null or _62 != '') and isalive=1 and export=#{export} and realname=#{realname} and checkpho=#{checkpho} " +
+            "and a.ctime BETWEEN #{bTime} AND  #{eTime} and right(name,11) like '1%' order by a.ctime ${sort} limit #{count} ")
     List<Map<String, String>> searchUsData(Map<String,Object> params);
 
     @Select("select a.id,a.name,a.psw,concat(left(a._62,50),'...') as _62,a.export,a.realname,a.checkpho,a.ctime,a.exporttime,a.ip," +
-            "b.ip as reip from wcuser a,wcphone b where (_62 is not null or _62 != '') and isalive=1 and export=#{export} and realname=#{realname} and checkpho=#{checkpho} " +
-            "and a.ctime BETWEEN #{bTime} AND  #{eTime} and right(name,12) like '44%' AND RIGHT(a.name,10) = right(b.phone,10) order by a.ctime ${sort} limit #{count} ")
+            "b.ip as reip from wcuser a LEFT JOIN wcphone b ON RIGHT (a.NAME, 10) = RIGHT(b.phone, 10) where (_62 is not null or _62 != '') and isalive=1 and export=#{export} and realname=#{realname} and checkpho=#{checkpho} " +
+            "and a.ctime BETWEEN #{bTime} AND  #{eTime} and right(name,12) like '44%' order by a.ctime ${sort} limit #{count} ")
     List<Map<String, String>> searchUkData(Map<String,Object> params);
 
 }
