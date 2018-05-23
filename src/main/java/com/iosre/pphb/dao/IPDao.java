e96835a3d878e81b2f78a42ab5479e26b174ecd1
liriming <594509671@qq.com>
2018-05-23 11:23:20 +0800

    系统配置

--- src/main/java/com/iosre/pphb/dao/IPDao.java
+++ src/main/java/com/iosre/pphb/dao/IPDao.java
@@ -10,6 +10,6 @@ public interface IPDao {
     @Insert("insert into ip(ip,address) values(#{ip},#{address})")
     Integer insertIP(@Param("ip") String ip,@Param("address") String address);
 
-    @Select("select if(count(1) > 0 , false, true) from ip where ip like CONCAT(#{ip},'%') AND ctime > date_sub(NOW(), INTERVAL 2 DAY)")
+    @Select("select if(count(1) > 0 , false, true) from ip where ip=#{ip} AND ctime > date_sub(NOW(), INTERVAL 2 DAY)")
     boolean checkIP(@Param("ip") String ip);
 }
